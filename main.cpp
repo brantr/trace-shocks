1ee89e6e4cd4d6df3c7139b8de9da9b7885447e8
Brant Robertson <brant.robertson@gmail.com>
2016-03-31 12:26:49 -0700

    Starting

--- main.cpp
+++ main.cpp
@@ -135,7 +135,15 @@ int main(int argc, char **argv)
   int n_times;
   float *times;
 
+  int n_redundancy = 1;
+
   time_A = timer();
+
+  if(argc<4)
+  {
+    printf("./trace_shocks imin imax ishock [fname] [n_redundancy]\n");
+    exit(-1);
+  }
 
   //if we've supplied a range of snapshots
   //to search, use that
@@ -145,8 +153,10 @@ int main(int argc, char **argv)
     imax   = atoi(argv[2]);
   	ishock = atoi(argv[3]);
   }
+  if(argc==6)
+    n_redundancy = atoi(argv[5]);
 
-  if(argc==5)
+  if(argc>=5)
   {
   	char fname_snap_list[200];
 
@@ -163,6 +173,7 @@ int main(int argc, char **argv)
    	}
 
    	fscanf(fp_snap_list,"%d\n",&n_snaps);
+    printf("n_snaps = %d\n",n_snaps);
 
     snap_list = (int *)malloc(n_snaps*sizeof(int));
 
@@ -186,7 +197,11 @@ int main(int argc, char **argv)
 
   //load snapshot times
   sprintf(fname_times,"times.txt");
-  fp_times = fopen(fname_times,"r");
+  if(!(fp_times = fopen(fname_times,"r")))
+  {
+    printf("Error opening %s.\n",fname_times);
+    exit(-1);
+  }
   fscanf(fp_times,"%d\n",&n_times);
   times = (float *) malloc(n_times*sizeof(float));
   for(int i=0;i<n_times;i++)
@@ -204,9 +219,11 @@ int main(int argc, char **argv)
   sprintf(fbint,"interactions");
   sprintf(fbase,"peak.blended");
   sprintf(fdtrace,"traces/");
-  sprintf(fint,"%s%s.%04d.%04d.txt",fdint,fbint,imin,imax);
-  sprintf(foutput,"%strace.%04d.%04d.%08d.txt",fdtrace,imin,imax,ishock);
-  sprintf(fbranch,"%sbranches.%04d.%04d.%08d.txt",fdtrace,imin,imax,ishock);
+
+  
+  sprintf(fint,"%s%s.%04d.%04d.%d.txt",fdint,fbint,imin,imax,n_redundancy);
+  sprintf(foutput,"%strace.%04d.%04d.%d.%08d.txt",fdtrace,imin,imax,n_redundancy,ishock);
+  sprintf(fbranch,"%sbranches.%04d.%04d.%d.%08d.txt",fdtrace,imin,imax,n_redundancy,ishock);
 
   read_interactions(fint,&ia);
 
@@ -215,91 +232,92 @@ int main(int argc, char **argv)
   long m;
 //  for(iA = imax; iA>imin; iA--)
 
+  //need to adjust islist to allow for 
+  //tracking of shocks in each of the redundant 
+  //snapshot times
+
   //the first list of shocks is just the
   //ishock of interest
   islist.push_back(ishock);
 
-  for(int i_snap = 0;i_snap<n_snaps-1; i_snap++)
+  for(int i_snap = 0;i_snap<n_snaps-n_redundancy; i_snap++)
   {
-  	iA = snap_list[i_snap];
-  	iB = snap_list[i_snap+1];
+  	iA = snap_list[i_snap]; 
 
-  	printf("iA %d iB %d\n",iA,iB);
-
-    sprintf(fcount,"%sinteraction_count.%04d.%04d.txt",fdint,iB,iA);
-
-    read_interaction_counts(fcount,&n_ints,&o_ints);
-
-    printf("******\n");
-    for(int ss=0;ss<islist.size();ss++)
+    for(int i_red=1;i_red <= n_redundancy; i_red++)
     {
-      ishock = islist[ss];
+      iB = snap_list[i_snap+i_red];
 
-      if(ishock>=n_ints.size())
+      printf("iA %d iB %d\n",iA,iB);
+      sprintf(fcount,"%sinteraction_count.%04d.%04d.txt",fdint,iB,iA);
+      read_interaction_counts(fcount,&n_ints,&o_ints);
+      printf("******\n");
+      for(int ss=0;ss<islist.size();ss++)
       {
-	printf("ERROR iA %04d iB %04d ishock %d n_ints.size() %ld\n",iA,iB,ishock,n_ints.size());
-	exit(-1);
+        ishock = islist[ss];
+
+        if(ishock>=n_ints.size())
+        {
+          printf("ERROR iA %04d iB %04d ishock %d n_ints.size() %ld\n",iA,iB,ishock,n_ints.size());
+          exit(-1);
+        }
+
+        //print information about interaction counts
+        printf("iA %04d iB %04d ishock %d n_ins %ld o_ints %ld\n",iA,iB,ishock,n_ints[ishock],o_ints[ishock]);
+
+        for(int i=0;i<n_ints[ishock];i++)
+        {
+          ia_tmp.push_back(ia[ioff + o_ints[ishock] + i]);
+          m = ia_tmp.size()-1;
+          printf("iA %04d i %10d nints %4ld idA %10ld idB %10ld nex %8ld frac_A %5.4e\n",ia_tmp[m].snap_A,i,n_ints[ishock],ia_tmp[m].id_A,ia_tmp[m].id_B,ia_tmp[m].n,ia_tmp[m].frac_A);
+        } //end loop over interactions
+      }// end loop over list of traced shocks
+
+      //reset the shock list
+      vector<long>().swap(islist);
+      for(int i=0;i<ia_tmp.size();i++)
+      {
+        ia_shock.push_back(ia_tmp[i]);
+        islist.push_back(ia_tmp[i].idx_B);
       }
 
-      printf("iA %04d iB %04d ishock %d n_ins %ld o_ints %ld\n",iA,iB,ishock,n_ints[ishock],o_ints[ishock]);
+      //remember the number of interactions
+      nbinteractions.push_back(ia_tmp.size());
 
-      for(int i=0;i<n_ints[ishock];i++)
-      {
-        ia_tmp.push_back(ia[ioff + o_ints[ishock] + i]);
-        m = ia_tmp.size()-1;
-        printf("iA %04d i %10d nints %4ld idA %10ld idB %10ld nex %8ld frac_A %5.4e\n",ia_tmp[m].snap_A,i,n_ints[ishock],ia_tmp[m].id_A,ia_tmp[m].id_B,ia_tmp[m].n,ia_tmp[m].frac_A);
-      }
-    }
+      //keep unique
+      std::sort(islist.begin(), islist.end());
+      il = std::unique(islist.begin(), islist.end());
+      islist.resize( std::distance(islist.begin(), il) );
 
+      for(int i=0;i<islist.size();i++)
+        printf("i %d islist[%d] %ld\n",i,i,islist[i]);
 
+      //if there are no interactions, break the loop
+      if(ia_tmp.size()==0)
+        break;
 
+      printf("HERE\n");
 
-    //reset the shock list
-    vector<long>().swap(islist);
-    for(int i=0;i<ia_tmp.size();i++)
-    {
-      ia_shock.push_back(ia_tmp[i]);
-      islist.push_back(ia_tmp[i].idx_B);
-    }
+      //remember the current number of branches
+      //nbranches.push_back(ia_tmp.size());
+      nbranches.push_back(islist.size());
+      printf("nbranches %ld\n",nbranches[nbranches.size()-1]);
 
-    //remember the number of interactions
-    nbinteractions.push_back(ia_tmp.size());
+      //reset temporary interaction list
+      vector<interaction>().swap(ia_tmp);
 
-    //keep unique
-  	std::sort(islist.begin(), islist.end());
-  	il = std::unique(islist.begin(), islist.end());
-  	islist.resize( std::distance(islist.begin(), il) );
+      //advance ioff
+      long n_int_sum = 0;
+      for(int i=0;i<n_ints.size();i++)
+        n_int_sum += n_ints[i];
 
-    for(int i=0;i<islist.size();i++)
-    	printf("i %d islist[%d] %ld\n",i,i,islist[i]);
+      //ioff += n_ints.size();
+      ioff += n_int_sum;
 
-
-
-
-    //if there are no interactions, break the loop
-    if(ia_tmp.size()==0)
-      break;
-
-  	printf("HERE\n");
-
-    //remember the current number of branches
-    //nbranches.push_back(ia_tmp.size());
-    nbranches.push_back(islist.size());
-    printf("nbranches %ld\n",nbranches[nbranches.size()-1]);
-
-    //reset temporary interaction list
-    vector<interaction>().swap(ia_tmp);
-
-    //advance ioff
-    long n_int_sum = 0;
-    for(int i=0;i<n_ints.size();i++)
-      n_int_sum += n_ints[i];
-    //ioff += n_ints.size();
-    ioff += n_int_sum;
-
-   	vector<long>().swap(n_ints);
-	vector<long>().swap(o_ints);  
-  }//end loop over snapshots
+      vector<long>().swap(n_ints);
+      vector<long>().swap(o_ints);
+    }//end loop over second snapshots
+  }//end loop over first snapshots
 
 
 
@@ -361,7 +379,7 @@ void write_branches(char fname[], vector<long> ia, vector<long> ib)
     printf("Error opening %s.\n",fname);
     exit(-1);
   }
-  fprintf(fp,"%ld\n",ia.size());
+  //fprintf(fp,"%ld\n",ia.size());
   for(size_t i=0;i<ia.size();i++)
   {
     fprintf(fp,"%ld\t%ld\n",ia[i],ib[i]);
@@ -376,7 +394,7 @@ void write_interactions(char fname[], vector<interaction> ia, float *times)
   	printf("Error opening %s.\n",fname);
   	exit(-1);
   }
-  fprintf(fp,"%ld\n",ia.size());
+  //fprintf(fp,"%ld\n",ia.size());
   for(size_t i=0;i<ia.size();i++)
   {
   	//fprintf(fp,"%04d %04d %9.8f %9.8f %8ld %8ld %8ld %5.4e %5.4e %10ld %8ld %8ld %5.4e %5.4e %5.4e %5.4e %10ld %8ld %8ld %5.4e %5.4e %5.4e %5.4e\n",ia[i].snap_A,ia[i].snap_B,times[ia[i].snap_A],times[ia[i].snap_B],ia[i].idx_A,ia[i].idx_B,ia[i].n,ia[i].frac_A,ia[i].frac_B,ia[i].id_A,ia[i].l_A,ia[i].o_A,ia[i].d_A,ia[i].x_A[0],ia[i].x_A[1],ia[i].x_A[2],ia[i].id_B,ia[i].l_B,ia[i].o_B,ia[i].d_B,ia[i].x_B[0],ia[i].x_B[1],ia[i].x_B[2]);
@@ -420,8 +438,8 @@ void read_interactions(char fname[], vector<interaction> *ia)
   	(*ia)[i].n = n;
   	(*ia)[i].frac_A = frac_A;
   	(*ia)[i].frac_B = frac_B;
-    (*ia)[i].frac_A = frac_A_dense;
-    (*ia)[i].frac_B = frac_B_dense;
+    (*ia)[i].frac_A_dense = frac_A_dense;
+    (*ia)[i].frac_B_dense = frac_B_dense;
   	(*ia)[i].snap_A = snap_A;
   	(*ia)[i].snap_B = snap_B;
   	(*ia)[i].idx_A = idx_A;
